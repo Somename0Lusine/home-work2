418f832c327e4f8bbbced03909be24444437346b
Lusine <lis.tim.11@mail.ru>
2022-08-04 15:57:19 +0300

    new fails

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000; // стоимость билета
+
+        int miles = 20;// одна милля за 20 рублей
+
+        int bonus =  price / miles ; // сумму делим на бонусные милли
+
+        System.out.println( " количество миль за покупку билета : "   + bonus );
+
+
+
+
+
+
+
+
+    }
+}
